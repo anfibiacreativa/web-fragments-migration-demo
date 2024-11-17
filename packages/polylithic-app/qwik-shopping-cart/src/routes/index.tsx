fb8f824d50d49ff04dc109cca9d1b475c01e0137
Natalia Venditto <nataliafrontend@gmail.com>
2024-11-17 11:49:34 +0100

    chore: add qwik shopping cart

--- packages/polylithic-app/qwik-shopping-cart/src/routes/index.tsx
+++ packages/polylithic-app/qwik-shopping-cart/src/routes/index.tsx
@@ -4,6 +4,7 @@ import { ShoppingCart } from '../components/shopping-cart/shopping-cart';
 export default component$(() => {
   return (
     <div>
+      <h1>Shopping Cart Web Fragment</h1>
       <ShoppingCart />
     </div>
   );
