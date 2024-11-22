029980f95dc71abde20cff754901089568b2c661
Natalia Venditto <nataliafrontend@gmail.com>
2024-11-22 14:50:10 +0100

    chore: add analog page

--- packages/polylithic-app/angular-shell-app/src/app/app.routes.ts
+++ packages/polylithic-app/angular-shell-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage-component/homepage.component';
 import { QwikPageComponent } from './components/qwik-page-component/qwik-page.component';
+import { AnalogPageComponent } from './components/analog-page-component/analog-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
-  { path: 'qwik-page', component: QwikPageComponent }
+  { path: 'qwik-page', component: QwikPageComponent },
+  { path: 'analog-page', component: AnalogPageComponent }
 ];
