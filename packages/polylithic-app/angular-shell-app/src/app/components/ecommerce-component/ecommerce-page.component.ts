9eed1e5c814db4d57bd8f3b74689adaa6a219b53
Natalia Venditto <nataliafrontend@gmail.com>
2024-11-22 16:17:33 +0100

    chore: add e-commerce page

---
+++ packages/polylithic-app/angular-shell-app/src/app/components/ecommerce-component/ecommerce-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+// import { FragmentComponent } from "../fragment-component/fragment.component";
+
+@Component({
+  selector: 'app-ecommerce-page',
+  standalone: true,
+  templateUrl: './ecommerce-page.component.html',
+  styleUrl: './ecommerce-page.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class EcommercePageComponent {
+
+}
