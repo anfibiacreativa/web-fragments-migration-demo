0ac943180209034f5493cdd768c75758534212d2
Natalia Venditto <nataliafrontend@gmail.com>
2024-11-22 14:38:02 +0100

    chore: fixed fragment fetching (yay!)

--- packages/polylithic-app/angular-shell-app/src/app/app.routes.ts
+++ packages/polylithic-app/angular-shell-app/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage-component/homepage.component';
+import { QwikPageComponent } from './components/qwik-page-component/qwik-page.component';
 
 export const routes: Routes = [
-  { path: 'qwik-page', component: HomepageComponent },
-  { path: 'analog-page', component: HomepageComponent },
+  { path: '', component: HomepageComponent },
+  { path: 'qwik-page', component: QwikPageComponent }
 ];
