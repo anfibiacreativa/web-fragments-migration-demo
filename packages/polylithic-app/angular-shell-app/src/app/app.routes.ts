c81fa63b0494f6748b5dcfa8d51efc4b21506a29
Natalia Venditto <nataliafrontend@gmail.com>
2024-11-22 16:17:33 +0100

    chore: add e-commerce page

--- packages/polylithic-app/angular-shell-app/src/app/app.routes.ts
+++ packages/polylithic-app/angular-shell-app/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage-component/homepage.component';
 import { QwikPageComponent } from './components/qwik-page-component/qwik-page.component';
 import { AnalogPageComponent } from './components/analog-page-component/analog-page.component';
+import { EcommercePageComponent } from './components/ecommerce-component/ecommerce-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'qwik-page', component: QwikPageComponent },
-  { path: 'analog-page', component: AnalogPageComponent }
+  { path: 'analog-page', component: AnalogPageComponent },
+  { path: 'ecommerce-page', component: EcommercePageComponent }
 ];
