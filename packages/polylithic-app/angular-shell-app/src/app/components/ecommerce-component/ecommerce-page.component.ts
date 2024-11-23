d0dde1b87ab57fe07eabf6d1f5b6c7fa7bc0f87a
Natalia Venditto <nataliafrontend@gmail.com>
2024-11-23 10:06:07 +0100

    chore: update html composed app page

--- packages/polylithic-app/angular-shell-app/src/app/components/ecommerce-component/ecommerce-page.component.ts
+++ packages/polylithic-app/angular-shell-app/src/app/components/ecommerce-component/ecommerce-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AnalogPageComponent } from "../analog-page-component/analog-page.component";
+import { QwikPageComponent } from "../qwik-page-component/qwik-page.component";
 // import { FragmentComponent } from "../fragment-component/fragment.component";
 
 @Component({
@@ -7,6 +9,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   templateUrl: './ecommerce-page.component.html',
   styleUrl: './ecommerce-page.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [AnalogPageComponent, QwikPageComponent],
 })
 export class EcommercePageComponent {
 
