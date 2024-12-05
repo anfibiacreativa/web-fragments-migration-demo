e11235db6888c376cb6ffb52a9155dd19a414b61
Igor Minar <iminar@cloudflare.com>
2024-12-05 17:32:30 +0100

    fix: remove debugger statements

--- packages/polylithic-app/angular-shell-app/src/app/app.component.ts
+++ packages/polylithic-app/angular-shell-app/src/app/app.component.ts
@@ -11,11 +11,9 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
 
   constructor(ngZone: NgZone) {
-      debugger;
       ngZone.runOutsideAngular(() => {
         // register web fragment custom elements (fragment-outlet and fragment-host)
         import('web-fragments/elements').then(({ register }) => {
-          debugger;
           register();
         }).catch((error) => {
           console.error('Error registering custom elements for web-fragments outlet and host:', error);
