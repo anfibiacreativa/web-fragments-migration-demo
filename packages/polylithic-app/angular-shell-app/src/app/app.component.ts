dff7f46c6db4916a55c3650e99a95c4b59f81647
Igor Minar <iminar@cloudflare.com>
2024-12-05 15:56:11 +0100

    fix(angular-shell-app): register web-fragment elements outside of NgZone
    This prevents unnecessary dirty checking in the legacy Angular app.

--- packages/polylithic-app/angular-shell-app/src/app/app.component.ts
+++ packages/polylithic-app/angular-shell-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(ngZone: NgZone) {
+      debugger;
+      ngZone.runOutsideAngular(() => {
+        // register web fragment custom elements (fragment-outlet and fragment-host)
+        import('web-fragments/elements').then(({ register }) => {
+          debugger;
+          register();
+        }).catch((error) => {
+          console.error('Error registering custom elements for web-fragments outlet and host:', error);
+        });
+      });
+  }
+
   title = 'Angular Migrated App | Web Fragments';
 }
--- packages/polylithic-app/angular-shell-app/src/main.ts
+++ packages/polylithic-app/angular-shell-app/src/main.ts
@@ -2,12 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-// register web fragment custom elements (fragment-outlet and fragment-host)
-import('web-fragments/elements').then(({ register }) => {
-  register();
-}).catch((error) => {
-  console.error('Error registering custom elements for web-fragments outlet and host:', error);
-});
-
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
