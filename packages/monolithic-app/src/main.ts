2ff687cf974726dee511ff2dcd0747806761fe62
Igor Minar <iminar@cloudflare.com>
2024-12-09 19:19:05 +0100

    fix: add 2sec slowdown to the monolithic app as well
    so that both versions are equal

--- packages/monolithic-app/src/main.ts
+++ packages/monolithic-app/src/main.ts
@@ -2,4 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig);
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+console.log('waiting 2 seconds to simulate a slow CSR app');
+wait(2000).then(() => {
+  console.log('done waiting, bootstraping the app shell');
+  bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+});
