4f52ac9607a3e8b0eb24f5b0ebbbce9bad63b60b
Natalia Venditto <nataliafrontend@gmail.com>
2025-01-21 11:45:07 +0100

    feat: add fontawesome to head using _document (and other improvements)

--- packages/monolithic-app-nextjs/pages/index.tsx
+++ packages/monolithic-app-nextjs/pages/index.tsx
@@ -10,11 +10,6 @@ const Home: React.FC = () => {
   const { isCartOpen, toggleCart } = useCartToggle(); // Toggle the cart sidebar
   return (
     <>
-      <Head>
-        <title>Next.js Monolithic App</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></link>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Layout>
         <div className='container'>
           <div className={isCartOpen ? 'backdrop visible' : 'backdrop'}></div>
