b2ae1ead3e05ee68daf1faeb7455fa474cc3887c
Natalia Venditto <nataliafrontend@gmail.com>
2025-03-06 14:20:29 +0100

    feat: add nuxt catalog site

---
+++ packages/polylithic-app/nuxt-product-catalog/nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  app: {
+    head: {
+      title: 'Nuxt Product Catalog',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  },
+  css: ['~/assets/styles.css']
+})
