78b9ca9971459704df9c05da11377dfa50ad3e9a
Natalia Venditto <nataliafrontend@gmail.com>
2025-03-06 14:20:29 +0100

    feat: add nuxt catalog site

---
+++ packages/polylithic-app/nuxt-product-catalog/components/ProductCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="product-card" @click="$emit('click')">
+    <img :src="product.imageUrl" :alt="product.name" />
+    <div class="content">
+      <h2>{{ product.name }}</h2>
+      <p>{{ product.description }}</p>
+
+      <!-- Star Rating Component -->
+      <StarRating :rating="product.rating" />
+
+      <div class="footer">
+        <span class="price">${{ product.price }}</span>
+        <button class="button" @click.stop>Add to Cart</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import type { Product } from '../types/types'; // Ensure the Product type is correctly imported
+import StarRating from './StarRating.vue';
+
+defineProps<{
+  product: Product;
+}>();
+</script>
