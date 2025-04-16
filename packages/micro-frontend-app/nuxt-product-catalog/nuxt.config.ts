5a7dd50c5608d0a7ee00379204e9b678f3003f68
Natalia Venditto <nataliafrontend@gmail.com>
2025-04-16 22:30:38 +0200

    ci: deploy to CG

--- packages/micro-frontend-app/nuxt-product-catalog/nuxt.config.ts
+++ packages/micro-frontend-app/nuxt-product-catalog/nuxt.config.ts
@@ -1,3 +1,5 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -17,16 +19,25 @@ export default defineNuxtConfig({
     '/': { redirect: '/store/catalog' },
   },
   nitro: {
+    preset: "cloudflare-pages",
+
+    cloudflare: {
+      nodeCompat: true
+    },
+
     output: {
       dir: 'dist',
-      publicDir: 'dist/public',
+      publicDir: 'dist',
     },
     publicAssets: [
       {
-        dir: 'dist/public',
+        dir: 'dist/public/_fragment/nuxt/assets',
         baseURL: '/_fragment/nuxt/assets/',
       }
     ]
   },
   css: ['~/assets/styles.css'],
+  modules: ["nitro-cloudflare-dev"]
 })
+
+
